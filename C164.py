19dd2c97cda67edfdadd37e42c090c65674d15da
ExtremeCoder991 <75404903+ExtremeCoder991@users.noreply.github.com>
2023-03-20 20:06:31 -0700

    Update C164.py

--- C164.py
+++ C164.py
@@ -15,9 +15,20 @@ def Open():
     img=ImageTk.PhotoImage(Image.open(img_path))
     label_image.configure(image=img)
     label_image.image=img
+def rotate():
+    print("Rotate")
+    print(img_path)
+    im=Image.open(img_path)
+    rotated_img=im.rotate(180)
+    img=ImageTk.PhotoImage(rotated_img)
+    label_image.configure(image=img)
+    label_image.image=img
     
 btn_open=Button(root,text="Open Image",command=Open,relief="flat",font=("Times New Roman0",15),bg="#743282",fg="white")
 btn_open.place(relx=0.5,rely=0.08,anchor=CENTER)
+
+btn_rotate=Button(root,text="Rotate Image",command=rotate,relief="flat",font=("Times New Roman0",15),bg="#808080",fg="white")
+btn_rotate.place(relx=0.5,rely=0.8,anchor=CENTER)
 
 label_image=Label(root,text="",bg="white",fg="white")
 label_image.pack()
